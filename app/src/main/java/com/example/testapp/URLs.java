904f08bff6ef69ad5fdd616d08ca75e66da98215
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2019-01-19 21:21:38 +0800

    add urls

---
+++ app/src/main/java/com/example/testapp/URLs.java
@@ -0,0 +1,11 @@
+package com.example.testapp;
+
+import static com.example.testapp.ApiKeys.API_KEY;
+
+public class URLs {
+    final static String TRENDING_WEEK = "https://api.themoviedb.org/3/trending/all/week?api_key="
+            + API_KEY;
+    final static String GENRE_LIST = "https://api.themoviedb.org/3/trending/all/week?api_key="
+            + API_KEY;
+    final static String IMAGE_BASE = "https://image.tmdb.org/t/p/w500/";
+}
