7545690d7e2ed78e73b696bfded0ad72f134d803
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2019-01-24 01:18:52 +0800

    update dao for live data

--- app/src/main/java/com/example/testapp/repository/MoviesDao.java
+++ app/src/main/java/com/example/testapp/repository/MoviesDao.java
@@ -4,6 +4,7 @@ import com.example.testapp.model.MovieItem;
 
 import java.util.List;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
@@ -12,10 +13,10 @@ import androidx.room.Query;
 public interface MoviesDao {
 
     @Insert
-    void insertAll(MovieItem... movieItems);
+    void insertAll(List<MovieItem> movieItems);
 
     @Query(value = "select * from movies;")
-    List<MovieItem> getMovies();
+    LiveData<List<MovieItem>> getMovies();
 
     @Query(value = "delete from movies;")
     void deleteAll();
--- app/src/main/java/com/example/testapp/repository/TvSeriesDao.java
+++ app/src/main/java/com/example/testapp/repository/TvSeriesDao.java
@@ -4,6 +4,7 @@ import com.example.testapp.model.TvSeriesItem;
 
 import java.util.List;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
@@ -12,10 +13,10 @@ import androidx.room.Query;
 public interface TvSeriesDao {
 
     @Insert
-    void insertAll(TvSeriesItem... tvSeriesItems);
+    void insertAll(List<TvSeriesItem> tvSeriesItems);
 
     @Query(value = "select * from tv_series;")
-    List<TvSeriesItem> getTvSeries();
+    LiveData<List<TvSeriesItem>> getTvSeries();
 
     @Query(value = "delete from tv_series;")
     void deleteAll();
