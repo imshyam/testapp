388eb642c500ace69200b1f31054cf86a967cd42
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2019-01-23 23:47:43 +0800

    refactor movie db

--- app/src/main/java/com/example/testapp/repository/MovieDatabase.java
+++ app/src/main/java/com/example/testapp/repository/MovieDatabase.java
@@ -9,21 +9,21 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 @Database(entities = MovieItem.class, version = 1)
-public abstract class MoviesDatabase extends RoomDatabase {
+public abstract class MovieDatabase extends RoomDatabase {
 
     public abstract MoviesDao moviesDao();
     private static final String DATABASE_NAME = "movies";
 
     private static final Object LOCK = new Object();
-    private static volatile MoviesDatabase moviesDatabase;
+    private static volatile MovieDatabase moviesDatabase;
 
-    public static MoviesDatabase getInstance(Context context) {
+    public static MovieDatabase getInstance(Context context) {
 
         if(moviesDatabase == null) {
             synchronized (LOCK) {
                 if(moviesDatabase == null) {
                     moviesDatabase = Room.databaseBuilder(context.getApplicationContext(),
-                            MoviesDatabase.class, DATABASE_NAME).build();
+                            MovieDatabase.class, DATABASE_NAME).build();
                 }
             }
         }
