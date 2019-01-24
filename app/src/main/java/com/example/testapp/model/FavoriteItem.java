9c88d430ae441e26ae553f3a9e65b6203a217738
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2019-01-24 11:18:06 +0800

    constructor

--- app/src/main/java/com/example/testapp/model/FavoriteItem.java
+++ app/src/main/java/com/example/testapp/model/FavoriteItem.java
@@ -1,13 +1,24 @@
 package com.example.testapp.model;
 
 import androidx.room.Entity;
+import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 @Entity(tableName = "favorite")
+@Data
+@AllArgsConstructor
 public class FavoriteItem {
     @PrimaryKey
     long id;
 
-    String itemId;
-    HistoryItemTypeEnum itemType;
+    private String itemId;
+    private HistoryItemTypeEnum itemType;
+
+    @Ignore
+    public FavoriteItem(String itemId, HistoryItemTypeEnum itemType) {
+        this.itemId = itemId;
+        this.itemType = itemType;
+    }
 }
