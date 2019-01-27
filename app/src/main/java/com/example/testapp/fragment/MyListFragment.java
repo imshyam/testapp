01d13845f72634c25e83690e9e47b15e5b035e38
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2019-01-27 14:26:16 +0800

    show saved data

--- app/src/main/java/com/example/testapp/fragment/MyListFragment.java
+++ app/src/main/java/com/example/testapp/fragment/MyListFragment.java
@@ -3,20 +3,31 @@ package com.example.testapp.fragment;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.testapp.OnFragmentInteractionListener;
 import com.example.testapp.R;
+import com.example.testapp.dao.FavoriteDao;
+import com.example.testapp.dao.MovieDatabase;
+import com.example.testapp.dao.MoviesDao;
+import com.example.testapp.executer.AppExecutor;
+import com.example.testapp.model.FavoriteItem;
+import com.example.testapp.viewmodel.TvMoviesViewModel;
+import com.example.testapp.viewmodel.ViewModelFactory;
+
+import java.util.concurrent.Executor;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link MyListFragment.OnFragmentInteractionListener} interface
+ * {@link OnFragmentInteractionListener} interface
  * to handle interaction events.
  * Use the {@link MyListFragment#newInstance} factory method to
  * create an instance of this fragment.
@@ -26,6 +37,8 @@ public class MyListFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    TextView tv;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -68,7 +81,31 @@ public class MyListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_list, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_my_list, container, false);
+        tv = rootView.findViewById(R.id.fav);
+        return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        MoviesDao moviesDao = MovieDatabase.getInstance(getActivity()).moviesDao();
+        FavoriteDao favoriteDao = MovieDatabase.getInstance(getActivity()).favoriteDao();
+        Executor executor = AppExecutor.getInstance().diskIO();
+
+        ViewModelFactory viewModelFactory = new ViewModelFactory(moviesDao, favoriteDao, executor);
+        TvMoviesViewModel viewModel = ViewModelProviders.of(this, viewModelFactory).get(TvMoviesViewModel.class);
+
+        viewModel.getFavorites().observe(this, favoriteItems -> {
+            String s = "";
+            for (FavoriteItem fav:  favoriteItems) {
+                s += fav.getItemType() +  " : " + fav.getItemId() + " \n";
+            }
+            tv.setText(s);
+        });
+
+
     }
 
     // TODO: Rename method, update argument and hook method into UI event
