d47ce1f1a7c5a5a201ff87e3bc5e56dfac3e70a4
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2019-01-29 22:46:07 +0800

    for correct id

--- app/src/main/java/com/example/testapp/dao/FavoriteDao.java
+++ app/src/main/java/com/example/testapp/dao/FavoriteDao.java
@@ -18,7 +18,9 @@ public interface FavoriteDao {
     @Query(value = "select * from favorite;")
     LiveData<List<FavoriteItem>> getAll();
 
-    @Query(value = "select * from movies_tv_series inner join favorite on favorite.itemId = movies_tv_series.id;")
+    @Query(value = "select mts.id, mts.poster_path, mts.original_title, mts.original_name, " +
+            "mts.adult, mts.genre_ids, mts.original_language, mts.overview, mts.popularity, mts.release_date, mts.vote_average" +
+            " from movies_tv_series as mts inner join favorite on favorite.itemId = mts.id;")
     LiveData<List<MovieItem>> getFavoriteMovies();
 
     @Query(value = "delete from favorite where itemId =:itemId;")
